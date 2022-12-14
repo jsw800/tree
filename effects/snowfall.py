e887f8495c947b22d72ab82f137ff4fa7098a15e
jesse <jesse.williams@limelyte.com>
2022-12-13 22:14:20 -0800

    snowfall effect is actually kinda nice now

--- effects/snowfall.py
+++ effects/snowfall.py
@@ -1,74 +1,71 @@
 import numpy as np
 import random
-from BaseEffect import BaseEffect
-
-BASE_COLOR = np.array([0, 0, 0], dtype=np.float64) / 255
-SNOW_COLOR = np.array([150, 150, 150], dtype=np.float64) / 255
-
-VELOCITY_MIN = -7
-VELOCITY_MAX = 7
-
-VELOCITY_Z_MIN = 5
-VELOCITY_Z_MAX = 15
-
-RADIUS_MIN = 50
-RADIUS_MAX = 70
-
-NUM_SPHERES = 8
+from BaseEffect import BaseEffect, ColorMode
 
 
-class Sphere:
-    def __init__(self, x, y, z):
-        self.initial = (x, y, z)
-        self.reset()
+VELOCITY_MIN = -0.002
+VELOCITY_MAX = 0.002
 
-    def reset(self):
-        self.x = self.initial[0]
-        self.y = self.initial[1]
-        self.z = self.initial[2]
-        self.velx = random.randint(VELOCITY_MIN, VELOCITY_MAX)
-        self.vely = random.randint(VELOCITY_MIN, VELOCITY_MAX)
-        self.velz = random.randint(VELOCITY_Z_MIN, VELOCITY_Z_MAX)
-        self.radius = random.randint(RADIUS_MIN, RADIUS_MAX)
+VELOCITY_Z_MIN = -0.03
+VELOCITY_Z_MAX = -0.08
 
-    def update(self):
-        self.x += self.velx
-        self.y += self.vely
-        self.z += self.velz
+RADIUS_MIN = 0.35
+RADIUS_MAX = 0.5
 
-    def point_in_sphere(self, xyz):
-        return self.dist(xyz) < self.radius
+NUM_SPHERES = 10
 
-    def dist(self, xyz):
-        return np.linalg.norm(np.array((self.x, self.y, self.z)) - xyz)
+"""
+self.spheres = [
+    [x, y, z, r, x_vel, y_vel, z_vel],
+    ...
+]    
+"""
 
 
 class Effect(BaseEffect):
     def __init__(self, points):
         super().__init__(points)
-        self.min_z = np.max(self.points[:, 2])
-        top_center = (
+        self.color_mode = ColorMode.HSV
+        self.max_z = np.max(self.points[:, 2])
+        self.hue = 0.0
+        self.top_center = np.array([
             (np.max(self.points[:, 0]) + np.min(self.points[:, 0])) / 2,
             (np.max(self.points[:, 1]) + np.min(self.points[:, 1])) / 2,
-            np.min(self.points[:, 2])
-        )
-        self.spheres = [Sphere(top_center[0], top_center[1], top_center[2]) for _ in range(NUM_SPHERES)]
+            np.max(self.points[:, 2]) + 1
+        ], dtype=np.float64)
+        self.spheres = np.zeros((NUM_SPHERES, 7), dtype=np.float64)
+        for i in range(NUM_SPHERES):
+            self.spheres[i] = self._gen_random_sphere()
+
+    def _gen_random_sphere(self):
+        return np.array([
+            random.uniform(-1, 1),
+            random.uniform(-1, 1),
+            self.max_z * 5 / 4,
+            random.uniform(RADIUS_MIN, RADIUS_MAX),
+            random.uniform(VELOCITY_MIN, VELOCITY_MAX),
+            random.uniform(VELOCITY_MIN, VELOCITY_MAX),
+            random.uniform(VELOCITY_Z_MIN, VELOCITY_Z_MAX),
+        ], dtype=np.float64)
 
     def update(self):
         # update spheres
-        for sphere in self.spheres:
-            sphere.update()
-            if sphere.z > self.min_z:
-                sphere.reset()
+        self.spheres[:, 0] += self.spheres[:, 4]
+        self.spheres[:, 1] += self.spheres[:, 5]
+        self.spheres[:, 2] += self.spheres[:, 6]
+
+        for i, sphere in enumerate(self.spheres):
+            if sphere[2] < -1:
+                self.spheres[i] = self._gen_random_sphere()
 
         # render out the colors
         self._reset_colors()
         for sphere in self.spheres:
             for i, point in enumerate(self.points):
-                if sphere.point_in_sphere(point):
-                    self.colors[i] = SNOW_COLOR * (sphere.dist(point) / sphere.radius)
+                dist = np.linalg.norm(sphere[:3] - point)
+                if dist < sphere[3]:
+                    self.colors[i, 2] += (sphere[3] - dist) / sphere[3]
+        self.colors[:, 2] = np.clip(self.colors[:, 2], 0, 0.4)
 
     def _reset_colors(self):
-        self.colors[:, 0] = BASE_COLOR[0]
-        self.colors[:, 1] = BASE_COLOR[1]
-        self.colors[:, 2] = BASE_COLOR[2]
+        self.colors[:, :] = 0.0
