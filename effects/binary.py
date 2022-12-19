ed46cee348cff640aba2068a3fea2540974e9095
jesse <jesse.williams@limelyte.com>
2022-12-18 17:36:23 -0800

    reverse binary

--- effects/binary.py
+++ effects/binary.py
@@ -9,15 +9,17 @@ COLOR = (0.4, 0.4, 0.4)
 class Effect(BaseEffect):
     def __init__(self, points):
         super().__init__(points)
-        self.shift = 0
+        num_leds = len(points)
+        self.shift = len(bin(num_leds)[2:])
         self.started = False
 
     def update(self):
+        if self.shift < 0:
+            return
         if self.started:
-            self.shift += 1
+            self.shift -= 1
             time.sleep(3)
         self.started = True
         onoff = (np.array(range(0, self.points.shape[0])) >> self.shift) & 1
-        print(np.array(range(0, self.points.shape[0])) >> self.shift)
         self.colors = np.array([COLOR if val else (0, 0, 0) for val in onoff])
 
